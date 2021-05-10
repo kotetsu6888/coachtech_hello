b9dd42a2e079e4bcab6891e036a0c54cac4f092c
onishitetsuhiro <kotetsu6888@gmail.com>
2021-05-10 23:46:30 +0900

    1

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="side_menu">
+    <input type="text" v-model="childText" />
+    <button @click="sendParent">親に送る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      childText:""
+    };
+  },
+  methods: {
+    sendParent() {
+      this.$emit("getChildText", this.childText);
+      this.childText = "";
+    }
+  }
+};
+</script>
+
